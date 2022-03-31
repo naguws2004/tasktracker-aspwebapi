2d75f1d23f5b9acd8722ea7ffb99f488a3594e4a
Nagesh Kumar Y <nageshkumar.y@gmail.com>
2022-03-31 08:21:08 +0530

    first commit

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using TaskWebAPI.Models;
+
+namespace TaskWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly TaskCRUD _taskCRUD;
+
+        public TasksController(IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetConnectionString("TaskTrackerConn"));
+
+            _configuration = configuration;
+            _taskCRUD = new TaskCRUD(mongoClient);
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(_taskCRUD.GetTasks());
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] Task task)
+        {
+            var result = _taskCRUD.InsertTask(task);
+            return new JsonResult(result);
+        }
+
+        [HttpPut]
+        public JsonResult Put([FromBody] Task task)
+        {
+            var result = _taskCRUD.UpdateTask(task);   
+            return new JsonResult(result);
+        }
+
+        [HttpDelete("{taskId:int}")]
+        public JsonResult Delete(int taskId)
+        {
+            var result = _taskCRUD.DeleteTask(taskId);
+            return new JsonResult(result);
+        }
+
+        [HttpPost]
+        [Route("UpdateTasks")]
+        public JsonResult UpdateTasks([FromBody] List<Task> tasks)
+        {
+            tasks = _taskCRUD.UpdateTasksMarker(tasks);
+
+            foreach (var task in tasks)
+            {
+                switch (task.Marker)
+                {
+                    case UpdateMarker.Insert:
+                        _taskCRUD.InsertTask(task);
+                        break;
+                    case UpdateMarker.Update:
+                        _taskCRUD.UpdateTask(task);
+                        break;
+                    case UpdateMarker.Delete:
+                        _taskCRUD.DeleteTask(task.TaskId);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return new JsonResult(_taskCRUD.UpdateTasksSuccessMessage());
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,159 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace TaskWebAPI.Models
+{
+    public enum UpdateMarker
+    {
+        Default = 0,
+        Insert = 1,
+        Update = 2,
+        Delete = 3
+    }
+
+    public class Task
+    {
+        [IgnoreDataMember]
+        public ObjectId Id { get; set; } = ObjectId.Empty;
+        public int TaskId { get; set; } = 0;
+        public string TaskName { get; set; } =  string.Empty;
+        public DateTime TaskDateTime { get; set; } = DateTime.MinValue;
+        public bool Remind { get; set; } = false;
+
+        [IgnoreDataMember]
+        public UpdateMarker Marker { get; set; } = UpdateMarker.Default; 
+    }
+
+    public class TaskCRUD
+    {
+        private readonly MongoClient _mongoClient;
+
+        public TaskCRUD(MongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
+        public string InsertTask(Task task)
+        {
+            int maxTaskId = GetMaxTaskId();
+            task.TaskId = maxTaskId + 1;
+            _mongoClient.GetDatabase("TaskTracker").GetCollection<Task>("Tasks").InsertOne(task);
+            return "New Task Added to Database Successfully.";
+        }
+
+        public string UpdateTask(Task task)
+        {
+            var updateFilter = Builders<Task>.Filter.Eq("TaskId", task.TaskId);
+            var update = Builders<Task>.Update.Set("Remind", task.Remind);
+            var updateResult = _mongoClient.GetDatabase("TaskTracker").GetCollection<Task>("Tasks").UpdateOne(updateFilter, update);
+            return UpdateResultMessage(updateResult);
+        }
+
+        public string DeleteTask(int taskId)
+        {
+            var deleteFilter = Builders<Task>.Filter.Eq("TaskId", taskId);
+            var deleteResult = _mongoClient.GetDatabase("TaskTracker").GetCollection<Task>("Tasks").DeleteOne(deleteFilter);
+            return DeleteResultMessage(deleteResult);
+        }
+
+        public List<Task> GetTasks()
+        {
+            var dbList = _mongoClient.GetDatabase("TaskTracker").GetCollection<Task>("Tasks").AsQueryable();
+            return dbList.ToList();
+        }
+
+        public string UpdateTasksSuccessMessage()
+        {
+            var dbList = GetTasks();
+            int taskCounter = dbList.Count();
+            return taskCounter.ToString() + " Tasks. Database Successfully Updated";
+        }
+
+        public List<Task> UpdateTasksMarker(List<Task> tasks)
+        {
+            var dbList = GetTasks();
+
+            foreach (var task in tasks)
+            {
+                if (!dbList.Any(x => x.TaskId == task.TaskId))
+                {
+                    // Mark for Insert
+                    task.Marker = UpdateMarker.Insert;
+                }
+                else if (dbList.Any(x => x.TaskId == task.TaskId))
+                {
+                    // Mark for Update
+                    task.Marker = UpdateMarker.Update;
+                }
+            }
+
+            foreach (var dbTask in dbList)
+            {
+                if (!tasks.Any(x => x.TaskId == dbTask.TaskId))
+                {
+                    // Add and Mark for Delete
+                    var task = dbTask;
+                    task.Marker = UpdateMarker.Delete;
+                    tasks.Add(task);
+                }
+            }
+
+            return tasks;
+        }
+
+        private int GetMaxTaskId()
+        {
+            var dbList = GetTasks();
+            return dbList.Max(x => x.TaskId);
+        }
+
+        private string UpdateResultMessage(UpdateResult updateResult)
+        {
+            if (updateResult.IsAcknowledged)
+            {
+                if (updateResult.MatchedCount > 0)
+                {
+                    if (updateResult.ModifiedCount > 0)
+                    {
+                        return "Task Updated in Database Successfully.";
+                    }
+                    else
+                    {
+                        return "Unable to Update Task.";
+                    }
+                }
+                else
+                {
+                    return "No matching Task found.";
+                }
+            }
+            else
+            {
+                return "Not Acknowledged by Database.";
+            }
+        }
+
+        private string DeleteResultMessage(DeleteResult deleteResult)
+        {
+            if (deleteResult.IsAcknowledged)
+            {
+                if (deleteResult.DeletedCount > 0)
+                {
+                    return "Task Deleted from Database Successfully.";
+                }
+                else
+                {
+                    return "No matching Task found.";
+                }
+            }
+            else
+            {
+                return "Not Acknowledged by Database.";
+            }
+        }
+    }
+}
